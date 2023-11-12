ad1cda273cf9658369cf949fdd7f6051f528602c
HamSangeok <hamsoo159@naver.com>
2023-11-13 01:13:06 +0900

    recommand_hospital

--- src/components/home/App.js
+++ src/components/home/App.js
@@ -1,20 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState} from "react";
 import axios from "axios";
 
 import RouterWeb from "./RouterWeb.js";
 import { silverDataAtom } from "../../recoil/silverDataAtom.js";
-import { useRecoilState, useSetRecoilState } from "recoil";
+import { relateDataAtom } from "../../recoil/relateDataAtom.js";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 
 function App() {
   const setSilverData = useSetRecoilState(silverDataAtom);
-  
-  useEffect(() => {
-      axios.get("http://localhost:8080/")
-      .then(res => {
-        setSilverData(res.data.silverList)
-      })
-      .catch(error => console.log(error))
-   }, []);
+  // useEffect(() => {
+  //   axios.get("http://localhost:8080/relate1?original_hospital=송추요양병원")
+  //   .then(res => {
+  //     console.log(res);
+      
+  //   })
+  //   .catch(error => console.log(error))
+  // }, []);
+      
+      useEffect(() => {
+        axios.get("http://localhost:8080")
+        .then(res => {
+          setSilverData(res.data.silverList)
+        })
+        .catch(error => console.log(error))
+      }, []);
 
   return (
     <div className="App">
